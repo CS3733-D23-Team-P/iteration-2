f5d64730e94b00173f0f6dd1e038cabf8aa2d789
SupermanBritt <supermanbritt03@comcast.net>
2023-04-17 17:26:36 -0400

    added filtering

--- src/main/java/edu/wpi/punchy_pegasi/frontend/components/PFXDropdown.java
+++ src/main/java/edu/wpi/punchy_pegasi/frontend/components/PFXDropdown.java
@@ -1,12 +1,34 @@
 package edu.wpi.punchy_pegasi.frontend.components;
 
-import org.controlsfx.control.SearchableComboBox;
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
-public class PFXDropdown<T> extends SearchableComboBox<T> {
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
 
-    public PFXDropdown() {
+public class PFXDropdown<T> extends ComboBox<T> {
+    ObservableList<T> items;
+    FilteredList<T> filteredItems;
+    BiPredicate<? super T, String> predicate;
+
+    public PFXDropdown(ObservableList<T> items, BiPredicate<? super T, String> predicate) {
         super();
-        this.getStyleClass().add("pfx-dropdown");
+        setEditable(true);
+        filteredItems = new FilteredList<>(items, p -> true);
+        this.items = items;
+        this.predicate = predicate;
+        getEditor().textProperty().addListener((obs, oldValue, newValue) -> {
+            final TextField editor = getEditor();
+            final T selected = getSelectionModel().getSelectedItem();
+            Platform.runLater(() -> {
+                if (selected == null || !selected.equals(editor.getText())) {
+                    filteredItems.setPredicate(i -> predicate.test(i, newValue));
+                }
+            });
+        });
+        setItems(filteredItems);
     }
-
 }
