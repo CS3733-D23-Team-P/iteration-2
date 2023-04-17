c15a4b0324d43971767af90138c9dc6820e27ab1
Piper O'Connell <proconnell@wpi.edu>
2023-04-17 13:10:52 -0400

    PFXDropdown addition

--- src/main/java/edu/wpi/punchy_pegasi/frontend/components/PFXDropdown.java
+++ src/main/java/edu/wpi/punchy_pegasi/frontend/components/PFXDropdown.java
@@ -1,11 +1,10 @@
 package edu.wpi.punchy_pegasi.frontend.components;
 
-import javafx.scene.control.ComboBox;
 import org.controlsfx.control.SearchableComboBox;
 
 public class PFXDropdown<T> extends SearchableComboBox<T> {
 
-    public PFXDropdown(){
+    public PFXDropdown() {
         super();
         this.getStyleClass().add("pfx-dropdown");
     }
